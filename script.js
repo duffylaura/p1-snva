3541897a324ac0d6ca6bdda381cfb2623c14b199
Coffee Pawz <lauralynneduffy@gmail.com>
2023-04-11 12:38:33 -0400

    Forgot to put a prevent default with event listener; that is why wasn't working

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 
-var firstName = document.getElementById("first-name").value; 
-var lastName = document.getElementById("last-name").value; 
-var dob = document.getElementById("dob").value; 
-var doj = document.getElementById("doj").value; 
+
+
 
 // FIRST write code to dynamically add HTML elements 
 // THEN add to local storage 
 // (See scrap.js for original idea)
-
 const list = document.getElementById("list");
 const btn = document.getElementById("btn");
 
-btn.addEventListener("click", function() {
+
+btn.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    
+    var firstName = document.getElementById("first-name").value; 
+    var lastName = document.getElementById("last-name").value; 
+    var dob = document.getElementById("dob").value; 
+    var doj = document.getElementById("doj").value; 
+
     const item = document.createElement("li");
     item.textContent = 
     `Name: ${firstName} ${lastName}
