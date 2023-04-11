f0b10535041337e2d75eb9d6abf40f871e3739a5
Coffee Pawz <lauralynneduffy@gmail.com>
2023-04-11 12:26:24 -0400

    stuck; get element by id is not working

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-//Global variables
-//use query selector to grab hold of information user enters 
-var firstName = document.querySelector("#first-name").value; 
-var lastName = document.querySelector("#last-name").value; 
-var dob = document.querySelector("#dob"); 
-var doj = document.querySelector("#doj"); 
+
+var firstName = document.getElementById("first-name").value; 
+var lastName = document.getElementById("last-name").value; 
+var dob = document.getElementById("dob").value; 
+var doj = document.getElementById("doj").value; 
 
 // FIRST write code to dynamically add HTML elements 
 // THEN add to local storage 
 // (See scrap.js for original idea)
 
-const container = document.getElementById("container");
 const list = document.getElementById("list");
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click", function() {
-  const nameItem = document.createElement("li");
-  nameItem.textContent = `Name: ${firstName} ${lastName}` ;
-  list.appendChild(nameItem); 
+    const item = document.createElement("li");
+    item.textContent = 
+    `Name: ${firstName} ${lastName}
+    DOB: ${dob}
+    DOJ: ${doj}` 
+    ;
+    list.appendChild(item); 
 });
 
