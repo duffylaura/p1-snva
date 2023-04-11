10259d27a4ca0f27ebfb97ad45befd767f3c1292
Coffee Pawz <lauralynneduffy@gmail.com>
2023-04-11 12:51:24 -0400

    Delete button added

--- version-1.js
+++ version-1.js
@@ -1,38 +1,24 @@
+
 // FIRST write code to dynamically add HTML elements 
 // THEN add to local storage 
 // (See scrap.js for original idea)
-
-//const list = document.getElementById("list");
-const container = document.getElementById("container"); 
+const list = document.getElementById("list");
 const btn = document.getElementById("btn");
 
 
 btn.addEventListener("click", function(event) {
     event.preventDefault(); 
-
+    
     var firstName = document.getElementById("first-name").value; 
     var lastName = document.getElementById("last-name").value; 
     var dob = document.getElementById("dob").value; 
     var doj = document.getElementById("doj").value; 
 
-    var area = document.createElement('textarea');
-    area.textContent = 
+    const item = document.createElement("li");
+    item.textContent = 
     `Name: ${firstName} ${lastName}
     DOB: ${dob}
     DOJ: ${doj}` 
     ;
-    container.appendChild(area); 
-
-    const deleteBtn = document.createElement('button'); 
-    deleteBtn.textContent = 'Delete'; 
-    container.appendChild(deleteBtn); 
-
-    deleteBtn.addEventListener ("click", function(event){
-        event.preventDefault(); 
-        container.removeChild(area); 
-        container.removeChild(deleteBtn); 
-    })
-
-
+    list.appendChild(item); 
 });
-
